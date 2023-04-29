Fixing problem with normalization in MIRD calculation (extra corporeal activity shuold not be used)

DICOM_RT/DicomPatient.py
@@ -583,7 +583,6 @@ class Patient3DActivity(DicomPatient):
             self.ReadPixelValuesSlices()
             self.Rescale()
             self.GetFrameOfReferenceSlices()
-        self.totalCounts = np.sum(self.img3D)
         
     def GetFrameOfReference3D(self):
         self.forUID = self.dcmFiles[0].FrameOfReferenceUID
@@ -683,6 +682,10 @@ class Patient3DActivity(DicomPatient):
                 new_file.is_little_endian = slices[0].is_little_endian
                 new_file.is_implicit_VR = slices[0].is_implicit_VR
                 new_file.save_as(filepath)
+
+    @property
+    def totalCounts(self):
+        return self.img3D.sum()
 
 class QoIDistribution:
     def __init__(self, array = None, quantity = None, unit = None):

MIRD/MIRDCalculator.py
@@ -39,7 +39,10 @@ class MIRDCalculator:
         self.Svalues = Svalues.SValuesData(radionuclide)
         self.accumulate = False
         
-    def CalculateOnActivityMapGrid(self, threshold = 0, tissue = 'Soft', normalize = False, accumulate = False):
+    def CalculateOnActivityMapGrid(self, threshold = 0, tissue = 'Soft', normalize = False, accumulate = False, usemask=True):
+        if usemask:
+            self.patCT.CreateBodyMask(write=False)
+            self.patActMap.ApplyValueOutsideMask(self.patCT.bodyMask, 0, self.patCT)
         shape = self.patActMap.img3D.shape
         self.doseAMGrid = np.zeros(shape)
         maxDistance = self.Svalues.maximumDistanceInVoxels